47f446aca27b8f311fdfd422cd5f11e64c24ff7c
Ryan Pethel <arpethel@gmail.com>
2019-04-03 08:45:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name, :student_grade, :student, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+# binding.pry
+  def sort(grade)
+    # binding.pry
+    @roster[grade].sort
+  end
+
+end
